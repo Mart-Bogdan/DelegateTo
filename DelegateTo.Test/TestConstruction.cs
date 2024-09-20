676449a34f88dfe404653d0272a3eb916374705c
Bohdan Mart <mart.bogdan@gmail.com>
2024-09-20 18:03:28 +0300

    Added tests for new features (inline and structs)

--- DelegateTo.Test/TestConstruction.cs
+++ DelegateTo.Test/TestConstruction.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using Xunit;
 using DelegateTo.Test;
 using DelegateTo;
@@ -9,14 +11,72 @@ public class TestConstruction
     [Fact]
     public void Test1()
     {
-        var parent = new Parent { Child = new() { X = 1 } };
+        var parent = new Parent { Child = new() { X = 1 }, Child2 = new Child2{ R = 42, }};
         Assert.Equal(1, parent.X);
         Assert.Equal(1, parent.A());
+        Assert.Equal(42, parent.R);
+        
+        var parent2 = new Parent2 { Child = new() { X = 1 }, Child2 = new Child2{ R = 43, } };
+        Assert.Equal(1, parent2.X);
+        Assert.Equal(1, parent2.A());
+        Assert.Equal(43, parent2.R);
     }
+    
+    
+    [Fact]
+    public void InlineAttributeShouldBePlacedIfAskedFor()
+    {
+        var parent = new InlineStruct { Child = new() { X = 1 } };
+        Assert.Equal(1, parent.X);
+        Assert.Equal(1, parent.A());
+        
+        var parent2 = new InlineClass { Child = new() { X = 1 } };
+        Assert.Equal(1, parent2.X);
+        Assert.Equal(1, parent2.A());
 
+        var method = typeof(InlineStruct).GetMethod(nameof(InlineStruct.A));
+        Assert.NotNull(method);
+        Assert.True((method.MethodImplementationFlags & MethodImplAttributes.AggressiveInlining) != 0);
+        
+        method = typeof(InlineClass).GetMethod(nameof(InlineClass.A));
+        Assert.NotNull(method);
+        Assert.True((method.MethodImplementationFlags & MethodImplAttributes.AggressiveInlining) != 0);
+        
+        method = typeof(Parent).GetMethod(nameof(Parent.A));
+        Assert.NotNull(method);
+        // not inlined!
+        Assert.True((method.MethodImplementationFlags & MethodImplAttributes.AggressiveInlining) == 0); 
+        
+        method = typeof(Parent2).GetMethod(nameof(Parent2.A));
+        Assert.NotNull(method);
+        // not inlined!
+        Assert.True((method.MethodImplementationFlags & MethodImplAttributes.AggressiveInlining) == 0); 
+        
+        method = typeof(InlineStruct).GetMethod("get_" + nameof(InlineStruct.X));
+        Assert.NotNull(method);
+        Assert.True((method.MethodImplementationFlags & MethodImplAttributes.AggressiveInlining) != 0);
+        
+        
+        method = typeof(InlineClass).GetMethod("get_" + nameof(InlineClass.X));
+        Assert.NotNull(method);
+        Assert.True((method.MethodImplementationFlags & MethodImplAttributes.AggressiveInlining) != 0);
+    }
 }
 
-partial class Parent
+
+partial struct InlineStruct
+{
+    [GenerateDelegate(Inline = true)]
+    public Child Child { get; set; }
+}
+
+partial class InlineClass
+{
+    [GenerateDelegate(Inline = true)]
+    public Child Child { get; set; }
+}
+
+partial struct Parent
 {
     [GenerateDelegate]
     public Child Child { get; set; }
